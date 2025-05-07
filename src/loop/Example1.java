2a8c6c87984bc595cde5f630211794f3f142381b
Yogita Chaudhari <yogitac211@gmail.com>
2025-05-07 09:05:41 +0530

    project_upload

---
+++ src/loop/Example1.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Example1 {
+
+	void printNumberInRev() {
+		for(int i =100; i>=1 ;i--) {
+			System.out.println(i);
+		}
+		
+	}
+	public static void main(String[] args) {
+		Example1 example1 =new Example1();
+		example1.printNumberInRev();
+
+	}
+
+}
